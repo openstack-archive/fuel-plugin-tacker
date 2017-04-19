7068a300df0c695fb4589bf504b29cbed970ba58
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-04-19 15:14:19 +0400

    Adjust tackerc to 3rd version of identity api
    Change-Id: Ia2de01863709201ac81b4ea32ddce348f4a2d799

--- deployment_scripts/puppet/manifests/tacker_rc.pp
+++ deployment_scripts/puppet/manifests/tacker_rc.pp
@@ -16,22 +16,25 @@ $auth_uri               = "${public_auth_protocol}://${public_auth_address}:5000
 $tackerc = inline_template("
 #!/bin/sh
 export LC_ALL=C
-export OS_NO_CACHE='true'
-export OS_TENANT_NAME='<%= @tacker_tenant %>'
-export OS_PROJECT_NAME='<%= @tacker_tenant %>'
-export OS_USERNAME='<%= @tacker_user %>'
-export OS_PASSWORD='<%= @tacker_user_password %>'
-export OS_AUTH_URL='<%= @auth_uri %>'
-export OS_DEFAULT_DOMAIN='default'
-export OS_AUTH_STRATEGY='keystone'
-export OS_REGION_NAME='RegionOne'
-export TACKER_ENDPOINT_TYPE='internalURL'
+export OS_NO_CACHE=true
+export OS_TENANT_NAME=<%= @tacker_tenant %>
+export OS_PROJECT_NAME=<%= @tacker_tenant %>
+export OS_USERNAME=<%= @tacker_user %>
+export OS_PASSWORD=<%= @tacker_user_password %>
+export OS_AUTH_URL=<%= @auth_uri %>
+export OS_IDENTITY_API_VERSION=3
+export OS_DEFAULT_DOMAIN=default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_STRATEGY=keystone
+export OS_REGION_NAME=RegionOne
+export TACKER_ENDPOINT_TYPE=internalURL
 ")
 
 file { '/root/tackerc':
+    ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0700',
-    ensure  => file,
-    content => "${tackerc}",
+    content => $tackerc,
 }
